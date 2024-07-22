fcdcdd886e4aad60818a608f575b96c1a41ab1d7
Pradeep Kumar <pradeep.kumar4@pathinfotech.com>
2024-07-22 22:37:35 +0530

    five commit

--- src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
+++ src/SimpleAPI/obj/Debug/net8.0/SimpleAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49bbe30dd3af27aabec103ddfdfb9fb3e95cffc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bea81f0e17e88a29e7f40756df209b4b56c3dea")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
